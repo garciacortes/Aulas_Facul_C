b3a5cde3295cbe670addf3f157bcd251d0f47328
Lucas Garcia <lucascortes2003@hotmail.com>
2022-10-20 18:55:33 -0300

    Exercicio3 com Funções/Repetições/Vetores

--- Lista2/Exercicio3.c
+++ Lista2/Exercicio3.c
@@ -2,50 +2,70 @@
 #include <stdio.h>
 #include <iso646.h>
 
+float VetorPos(float Notas[3], float Pesos[3]) {
+    float CopiarNotas[2], CopiarPesos[2];
+
+    CopiarNotas[0] = Notas[1];
+    CopiarNotas[1] = Notas[2];
+    Notas[2] = Notas[0];
+    Notas[1] = CopiarNotas[1];
+    Notas[0] = CopiarNotas[0];
+    CopiarPesos[0] = Pesos[1];
+    CopiarPesos[1] = Pesos[2];
+    Pesos[2] = Pesos[0];
+    Pesos[0] = CopiarPesos[0];
+    Pesos[1] = CopiarPesos[1];
+
+    return Notas[3], Pesos[3];
+}
+
+int VerificarPeso(float PesoA, float PesoB, float PesoC, float Notas[3], float Ponderada, int NotPass, int Posicao) {
+    if (PesoA > PesoB and PesoA > PesoC) {
+        printf("\nA Media Ponderada das Notas %.2f, %.2f, %.2f sera: %.2f.\n", Notas[0], Notas[1], Notas[2], Ponderada);
+        printf("A Nota %d (%.2f) e a maior nota apos o Calculo do Peso %d (%.2f).", Posicao, Notas[0], Posicao, PesoA);
+        return NotPass = 1;
+    }
+    else if (PesoA == PesoB and PesoA > PesoC) {
+        printf("\nA Media Ponderada das Notas %.2f, %.2f, %.2f sera: %.2f.\n", Notas[0], Notas[1], Notas[2], Ponderada);
+        printf("As Notas 1 (%.2f) e 2 (%.2f) foram as maiores notas apos o calculo do peso 1 (%.2f) e peso 2 (%.2f)", Notas[0], Notas[1], PesoA, PesoB);
+    }
+    else {
+        return NotPass;
+    }
+}
+
 int main() {
-    float Nota1, Nota2, Nota3, Ponderada, Peso1, Peso2, Peso3;
+    float Notas[3], Ponderada, Pesos[3];
+    int NotPass, i, x, Posicao;
 
     system("cls");
+    x = 1;
+    for (i = 0; i < 3; i++) {
+        printf("digite as Tres Notas, 1 por vez: ");
+        scanf("%f", &Notas[i]);
+        Pesos[i] = Notas[i] * x;
+        x++;
+        Ponderada = Ponderada + Pesos[i];
+    }
+    Ponderada = Ponderada / 6;
 
-    printf("digite a Nota 1: ");
-    scanf("%f", &Nota1);
-    printf("digite a Nota 2: ");
-    scanf("%f", &Nota2);
-    printf("digite a Nota 3: ");
-    scanf("%f", &Nota3);
-    Peso1 = Nota1 * 1;
-    Peso2 = Nota2 * 2;
-    Peso3 = Nota3 * 3;
-    Ponderada = ((Peso1 + Peso2 + Peso3) / 6);
+    Posicao = 1;
+    while (1) {
+        NotPass = VerificarPeso(Pesos[0], Pesos[1], Pesos[2], Notas, Ponderada, 0, Posicao);
+        if (NotPass == 0) {
+            Posicao++;
+            VetorPos(Notas, Pesos);
+        }
+        else {
+            break;
+        }
+    }
 
-    if (Peso1 > Peso2 and Peso1 > Peso3) {
-        printf("\nA Media Ponderada das Notas %.2f, %.2f, %.2f sera: %.2f.\n", Nota1, Nota2, Nota3, Ponderada);
-        printf("A Nota 1 (%.2f) e a maior nota apos o Calculo do Peso 1 (%.2f).", Nota1, Peso1);
-    }
-    else if (Peso2 > Peso1 and Peso2 > Peso3) {
-        printf("\nA Media Ponderada das Notas %.2f, %.2f, %.2f sera: %.2f.\n", Nota1, Nota2, Nota3, Ponderada);
-        printf("A Nota 2 (%.2f) e a maior nota apos o Calculo do Peso 2 (%.2f).", Nota2, Peso2);
-    }
-    else if (Peso3 > Peso1 and Peso3 > Peso2) {
-        printf("\nA Media Ponderada das Notas %.2f, %.2f, %.2f sera: %.2f.\n", Nota1, Nota2, Nota3, Ponderada);
-        printf("A Nota 3 (%.2f) e a maior nota apos o Calculo do Peso 3 (%.2f).", Nota3, Peso3);
-    }
-    else if (Peso1 == Peso2 and Peso1 > Peso3) {
-        printf("\nA Media Ponderada das Notas %.2f, %.2f, %.2f sera: %.2f.\n", Nota1, Nota2, Nota3, Ponderada);
-        printf("As Notas 1 (%.2f) e 2 (%.2f) foram as maiores notas apos o calculo do peso 1 (%.2f) e peso 2 (%.2f)", Nota1, Nota2, Peso1, Peso2);
-    }
-    else if (Peso1 == Peso3 and Peso1 > Peso2) {
-        printf("\nA Media Ponderada das Notas %.2f, %.2f, %.2f sera: %.2f.\n", Nota1, Nota2, Nota3, Ponderada);
-        printf("As Notas 1 (%.2f) e 3 (%.2f) foram as maiores notas apos o calculo do peso 1 (%.2f) e peso 3 (%.2f)", Nota1, Nota3, Peso1, Peso3);
-    }
-    else if (Peso2 == Peso3 and Peso2 > Peso1) {
-        printf("\nA Media Ponderada das Notas %.2f, %.2f, %.2f sera: %.2f.\n", Nota1, Nota2, Nota3, Ponderada);
-        printf("As Notas 2 (%.2f) e 3 (%.2f) foram as maiores notas apos o calculo do peso 2 (%.2f) e peso 3 (%.2f)", Nota2, Nota3, Peso2, Peso3);
-    }
+    /*
     else {
         printf("\nA Media Ponderada das Notas %.2f, %.2f, %.2f sera: %.2f.\n", Nota1, Nota2, Nota3, Ponderada);
         printf("As tres Notas foram iguais.");
-    }
+    }*/
 
     return 0;
 }
