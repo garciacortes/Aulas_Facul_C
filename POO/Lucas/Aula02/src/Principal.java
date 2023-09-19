a4a72cd3b621cb80502a48fb78d5aa7bdcb79d82
garciacortes <55157556+garciacortes@users.noreply.github.com>
2023-09-19 20:11:54 -0300

    Update Principal.java

--- POO/Lucas/Aula02/src/Principal.java
+++ POO/Lucas/Aula02/src/Principal.java
@@ -5,25 +5,36 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		Aluno []vetorObjetoAluno;
+		
+		System.out.println("Digite a quantidade de Alunos: ");
+		int qtdeAlunos = scan.nextInt();
+		
+		vetorObjetoAluno = new Aluno[qtdeAlunos];
 		
 		String nome;
 		int nota1, nota2, nota3;
 		
-		System.out.println("********* Dados do Aluno *********");
-		System.out.printf("Digite o nome do aluno: ");
-		nome = scan.next();
-		System.out.printf("Digite a nota 1: ");
-		nota1 = scan.nextInt();
-		System.out.printf("Digite a nota 2: ");
-		nota2 = scan.nextInt();
-		System.out.printf("Digite a nota 3: ");
-		nota3 = scan.nextInt();
+		for (int i = 0; i < qtdeAlunos; i++) {
+			System.out.println("********* Dados do Aluno ********");
+			System.out.println("Digite o nome do aluno: ");
+			nome = scan.next();
+			System.out.println("Digite a nota 1: ");
+			nota1 = scan.nextInt();
+			System.out.println("Digite a nota 2: ");
+			nota2 = scan.nextInt();
+			System.out.println("Digite a nota 3: ");
+			nota3 = scan.nextInt();
+			
+			//INSTANCIAR O OBJETO PARA CADA POSIÇÃO DO VETOR
+			vetorObjetoAluno[i] = new Aluno(nome, nota1, nota2, nota3);
+		}
 		
-		Aluno objetoAluno; // declaração do objeto
-		objetoAluno = new Aluno(nome, nota1, nota2, nota3);
-		
-		System.out.println("********* Boletim Final ********");
-		System.out.println("Aluno " + objetoAluno.RetornaNome() + " esta " + objetoAluno.ResultadoFinal());
+		System.out.println("********** Boletim Final ********** ");
+		for (int i = 0; i < qtdeAlunos; i++) {
+			System.out.println("Aluno " + vetorObjetoAluno[i].RetornaNome() + " está " + vetorObjetoAluno[i].ResultadoFinal());
+		}
+
 	}
 
 }
