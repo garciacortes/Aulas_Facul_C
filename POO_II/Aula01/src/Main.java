e650612cbebfa1a146a5de253fe1177b4b8ed1d4
garciacortes <55157556+garciacortes@users.noreply.github.com>
2024-02-27 18:08:17 -0300

    Update Main.java
    adicionado o Exerc1 com as modificações
    Adicionado o Exerc2 Feita na data 27/02/2024

--- POO_II/Aula01/src/Main.java
+++ POO_II/Aula01/src/Main.java
@@ -1,11 +1,12 @@
-import View.TelaView;
+import View.Exerc1;
+import View.Exerc2;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		TelaView frame = new TelaView();
-		frame.setVisible(true);
+		Exerc1 frame = new Exerc1();
+		Exerc2 frame2 = new Exerc2();
 	}
 
 }
